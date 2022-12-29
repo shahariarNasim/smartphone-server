b28b94a3c70de982689bd37b2499847430099f89
Shahariar Nasim <shahariar.nasim2017@gmail.com>
2022-12-30 02:04:22 +0600

    gg

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lgvfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://smartphone:mE9g3k6zCDwgDl7L@cluster0.lgvfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
